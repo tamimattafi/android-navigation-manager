81a8de22566d44e39eacbf03e3f7e659f67fb34d
Tamim Attafi <tamimattafi@iMac-Tamim.local>
2021-09-20 18:11:25 +0700

    update: Updated library version

--- buildSrc/src/main/kotlin/AndroidConfiguration.kt
+++ buildSrc/src/main/kotlin/AndroidConfiguration.kt
@@ -2,6 +2,6 @@ object AndroidConfiguration {
     const val COMPILE_SDK_VERSION = 29
     const val MIN_SDK_VERSION = 14
     const val TARGET_SDK_VERSION = 29
-    const val VERSION_CODE = 36
-    const val VERSION_NAME = "2.1.1"
+    const val VERSION_CODE = 37
+    const val VERSION_NAME = "2.1.2"
 }
