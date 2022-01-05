f130d26976c21840550e5d119dbefc696d6757e9
Tamim <tamim@MacBook-Pro-Diana.local>
2022-01-06 00:47:46 +0700

    downgrade: Decrese target sdk version

--- buildSrc/src/main/kotlin/AndroidConfiguration.kt
+++ buildSrc/src/main/kotlin/AndroidConfiguration.kt
@@ -1,7 +1,7 @@
 object AndroidConfiguration {
-    const val COMPILE_SDK_VERSION = 31
+    const val COMPILE_SDK_VERSION = 30
     const val MIN_SDK_VERSION = 14
-    const val TARGET_SDK_VERSION = 31
+    const val TARGET_SDK_VERSION = 30
     const val VERSION_CODE = 39
-    const val VERSION_NAME = "2.1.4"
+    const val VERSION_NAME = "2.1.5"
 }
